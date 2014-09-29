b2879a41d7ea005e7fb83087da11a96de977c441
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-09-28 20:03:16 -0400

    adding check for birthday

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,15 +20,23 @@ angular.module('birthdayAppApp')
     $scope.login = function() {
       // From now on you can use the Facebook service just as Facebook api says
       Facebook.login(function(response) {
+        console.log(response);
         Facebook.api('/me',function (response) {
           $scope.isLoggedIn = true;
           $scope.user = response;
+          var numberOfDays = numberofDayTilBirthday($scope.user);
+          if(numberOfDays === 0){
+            $scope.isBirthday = true;
+          } else {
+            $scope.isBirthday = false;
+            $scope.numberOfDays = numberOfDays;
+          }
           console.log(response);
-        })
+        });
         Facebook.api('/me/photos',function  (response) {
           console.log(response);
           $scope.photos = response;
-        })
+        });
       },{scope:'public_profile,user_birthday,user_photos'});
     };
 
@@ -46,13 +54,43 @@ angular.module('birthdayAppApp')
       Facebook.logout(function (response) {
         console.log(response);
         $scope.isLoggedIn = false;
-      })
-    }
+      });
+    };
 
     $scope.me = function() {
       Facebook.api('/me', function(response) {
         $scope.user = response;
       });
     };
+
+    function numberofDayTilBirthday(user){
+      var birthday = user.birthday;
+      var birthdayArray = birthday.split('/');
+      var birthdayMonth = parseInt(birthdayArray[0]);
+      var birthdayDay = parseInt(birthdayArray[1]);
+      var birthdayYear = parseInt(birthdayArray[2]);
+      birthday = new Date(birthdayYear,birthdayMonth-1,birthdayDay);
+
+      var myBirthday, today, bday, diff, days;
+      myBirthday = [birthdayDay,birthdayMonth]; // 6th of February
+      today = new Date();
+      bday = new Date(today.getFullYear(),myBirthday[1]-1,myBirthday[0]);
+      if( today.getTime() > bday.getTime()) {
+        bday.setFullYear(bday.getFullYear()+1);
+      }
+      diff = bday.getTime()-today.getTime();
+      days = Math.floor(diff/(1000*60*60*24));
+
+      var now = new Date();
+      var month = now.getMonth() + 1;
+      var day = now.getDate();
+      // var year = now.getFullYear();
+
+      if(parseInt(month) === parseInt(birthdayMonth) && parseInt(day) === parseInt(birthdayDay)){
+        return 0;
+      } else {
+        return days;
+      }
+    }
 
 });
